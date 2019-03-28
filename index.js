79998eebf9b7bc638854a76a8804c50abe5d2e40
Lei Gao <greenberet1945@gmail.com>
2019-03-28 15:44:21 +1100

    compile multiple files per request

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const wdDir="/tmp/wd";
 
 exports.handler = (event, context, callback) => {
 
-  if( ! event.script )
+  if( ! event.scripts )
   {
-    callback("missing script");
+    callback("missing scripts");
   }
 
   if( fs.existsSync( wdDir)==false)
@@ -68,136 +68,124 @@ function lock( filename)
     return false;
   }
 }
-function perform(event, context, callback)
-{
-  const srcDir=wdDir+"/src";
-  const distDir =wdDir+"/dist";
-  deleteFolderRecursive( srcDir);
-  deleteFolderRecursive( distDir);
-  const outFile=distDir + "/bundle.js";
+function perform(event, context, callback){
+    const scripts = event.scripts;
+    const result = [];
+    for(let i = 0;i < scripts.length;i++){
+        const script = scripts[i];
+        const srcDir=wdDir+"/src";
+        const distDir =wdDir+"/dist";
+        deleteFolderRecursive( srcDir);
+        deleteFolderRecursive( distDir);
+        const outFile=distDir + "/bundle.js";
 
-  cp.spawnSync( "cp",["-a","--no-clobber", "./node_modules", wdDir]);
-  cp.spawnSync( "cp",["-a","--no-clobber", "./package-lock.json", wdDir]);
-  cp.spawnSync( "cp",["./webpack.config.js", wdDir]);
-  let currentPackageJSON={};
-  if( fs.existsSync( wdDir +'/package.json'))
-  {
-      currentPackageJSON=JSON.parse(fs.readFileSync(wdDir +'/package.json', 'utf8'));
-  }
-  let newPackageJSON=JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-  if( event.devDependencies)
-  {
-    //console.info( JSON.stringify( packageJSON, null, 2));
-    //let packageJSON= JSON.parse(JSON.stringify(newPackageJSON));
-    newPackageJSON.devDependencies=Object.assign( newPackageJSON.devDependencies,event.devDependencies);
-    //console.info( JSON.stringify( packageJSON, null, 2));
-    //newPackageJSON=packageJSON;
-    fs.writeFileSync( wdDir +'/package.json', JSON.stringify(newPackageJSON,null, 2));
-  }
-  else {
-    cp.spawnSync( "cp",["./package.json", wdDir]);
-  }
-// console.info( "vvvvvvvvvvvvvvvvvvvvvvvvvv");
-// console.info( JSON.stringify(JSON.parse(fs.readFileSync(wdDir +'/package.json', 'utf8')),null, 2));
-// console.info( "^^^^^^^^^^^^^^^^^^^^^^^^^^");
-// console.log( JSON.stringify(process.env, null, 2));
-// console.log( JSON.stringify(process.argv, null, 2));
-// ls( "./");
-// ls( "/tmp");
-    // pwd();
-  if(
-  //  fs.existsSync( wdDir +'/node_modules') ==false ||
-    JSON.stringify( currentPackageJSON)!= JSON.stringify( newPackageJSON)
-  )
-  {
-
-    const nodeCmd=process.argv[0];
-    const npmCmd=process.cwd() + "/node_modules/npm/bin/npm-cli.js";
-    console.info( nodeCmd + " " + npmCmd + " install");
-
-    let p=cp.spawnSync(
-      nodeCmd,
-      [npmCmd, "install", "--ignore-scripts", "--yes"],
-      // ["--version"],
-      // [process.env.HOME +"/node_modules/npm/bin/npm-cli.js","install"],
-      {
-        cwd:wdDir,
-        shell:true,
-        env:{
-          HOME:"/tmp/wd"
-        }
-      }
-    );
-
-    console.info( "pid: " + p.pid +", status: " + p.status + ", signal: " + p.signal);
-    var out=p.stdout.toString();
-    if( out != "")
-    {
-      console.info( "[STDOUT]" + out);
-    }
-    var err=p.stderr.toString();
-    if( err !="")
-    {
-      console.error( "[STDERR]" + err);
-    }
-    //
-    // ls( wdDir);
-    // ls( wdDir+"/node_modules/react-select");
-  }
-
-  if(
-    fs.existsSync( wdDir +'/node_modules') ==false
-  )
-  {
-    return callback("no node modules installed");
-  }
-// return callback(null, "hack");
-  fs.mkdirSync(srcDir);
-  fs.appendFileSync(srcDir + '/index.js', event.script);
-  //var wp = spawn('./node_modules/.bin/webpack', ['--config', 'webpack.config.js', '--mode', 'production']);
-//  var wp = spawn('npm', ['run-script', 'build']);
-  var wp = exec(
-  //  'npm run-script build',
-    './node_modules/.bin/webpack --config webpack.config.js --mode production',
-    {
-        cwd:wdDir
-    },
-    (error, stdout, stderr) => {
-      if (error ) {
-        var msg='[ERROR]: "' + error.name + '" - ' + error.message + '\n' + stderr;
-        msg=msg.trim();
-        console.error( msg );
-        callback(msg);
-        return;
-      }
-
-      if( fs.existsSync( outFile))
-      {
-        try{
-          callback(
-            null,
-            fs.readFileSync(outFile, 'utf8')
-          );
-        }
-        catch( e)
+        cp.spawnSync( "cp",["-a","--no-clobber", "./node_modules", wdDir]);
+        cp.spawnSync( "cp",["-a","--no-clobber", "./package-lock.json", wdDir]);
+        cp.spawnSync( "cp",["./webpack.config.js", wdDir]);
+        let currentPackageJSON={};
+        if( fs.existsSync( wdDir +'/package.json'))
         {
-          callback( "could not read: " + e);
+            currentPackageJSON=JSON.parse(fs.readFileSync(wdDir +'/package.json', 'utf8'));
         }
-      }
-      else {
-        callback( "No file: " + outFile);
-      }
+        let newPackageJSON=JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+        if( event.devDependencies)
+        {
+          //console.info( JSON.stringify( packageJSON, null, 2));
+          //let packageJSON= JSON.parse(JSON.stringify(newPackageJSON));
+          newPackageJSON.devDependencies=Object.assign( newPackageJSON.devDependencies,event.devDependencies);
+          //console.info( JSON.stringify( packageJSON, null, 2));
+          //newPackageJSON=packageJSON;
+          fs.writeFileSync( wdDir +'/package.json', JSON.stringify(newPackageJSON,null, 2));
+        }
+        else {
+          cp.spawnSync( "cp",["./package.json", wdDir]);
+        }
+      // console.info( "vvvvvvvvvvvvvvvvvvvvvvvvvv");
+      // console.info( JSON.stringify(JSON.parse(fs.readFileSync(wdDir +'/package.json', 'utf8')),null, 2));
+      // console.info( "^^^^^^^^^^^^^^^^^^^^^^^^^^");
+      // console.log( JSON.stringify(process.env, null, 2));
+      // console.log( JSON.stringify(process.argv, null, 2));
+      // ls( "./");
+      // ls( "/tmp");
+          // pwd();
+        if(
+        //  fs.existsSync( wdDir +'/node_modules') ==false ||
+          JSON.stringify( currentPackageJSON)!= JSON.stringify( newPackageJSON)
+        )
+        {
+
+          const nodeCmd=process.argv[0];
+          const npmCmd=process.cwd() + "/node_modules/npm/bin/npm-cli.js";
+          console.info( nodeCmd + " " + npmCmd + " install");
+
+          let p=cp.spawnSync(
+            nodeCmd,
+            [npmCmd, "install", "--ignore-scripts", "--yes"],
+            // ["--version"],
+            // [process.env.HOME +"/node_modules/npm/bin/npm-cli.js","install"],
+            {
+              cwd:wdDir,
+              shell:true,
+              env:{
+                HOME:"/tmp/wd"
+              }
+            }
+          );
+
+          console.info( "pid: " + p.pid +", status: " + p.status + ", signal: " + p.signal);
+          var out=p.stdout.toString();
+          if( out != "")
+          {
+            console.info( "[STDOUT]" + out);
+          }
+          var err=p.stderr.toString();
+          if( err !="")
+          {
+            console.error( "[STDERR]" + err);
+          }
+          //
+          // ls( wdDir);
+          // ls( wdDir+"/node_modules/react-select");
+        }
+
+        if(
+          fs.existsSync( wdDir +'/node_modules') ==false
+        )
+        {
+            return callback("no node modules installed");
+        }
+      // return callback(null, "hack");
+        fs.mkdirSync(srcDir);
+        fs.appendFileSync(srcDir + '/index.js', script);
+        //var wp = spawn('./node_modules/.bin/webpack', ['--config', 'webpack.config.js', '--mode', 'production']);
+      //  var wp = spawn('npm', ['run-script', 'build']);
+        var wp = cp.execSync(
+        //  'npm run-script build',
+          './node_modules/.bin/webpack --config webpack.config.js --mode production',
+          {
+              cwd:wdDir
+          });
+            if (fs.existsSync(outFile))
+            {
+                try{
+                    result.push(fs.readFileSync(outFile, 'utf8'));
+                    fs.unlinkSync(outFile);
+                }
+                catch (e)
+                {
+                    callback("could not read: " + e);
+                    return;
+                }
+            }
+            else {
+                callback("No file: " + outFile);
+                return;
+            }
+        //var wp = spawn('pwd');
+
+        console.info( wp );
     }
-  );
-  //var wp = spawn('pwd');
-
-  wp.stdout.on('data', function(data){
-    console.info( data.toString() );
-  });
-
-  wp.stderr.on('data', function(err){
-    console.error( err.toString() );
-  });
+    return callback(null, result);
 };
 
 function deleteFolderRecursive(path) {
